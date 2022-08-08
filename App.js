1f754ff7091e71d3960014909dfa75c59ec0e7c1
Evan-cell <60639510+Evan-cell@users.noreply.github.com>
2022-08-08 14:45:28 +0300

    txt input

--- App.js
+++ App.js
@@ -1,11 +1,37 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, Text, View, Button, TextInput} from 'react-native';
+
 
 export default function App() {
+  const [user, setUser] = useState('KIMANI')
+  const [shoe,setShoe] = useState({name:'guchi',price: 8})
+  const changeUser = () => {
+    setUser('MBWAK')
+    setShoe({name: 'ankara', price: 1200})
+  }
+  console.log(shoe)
   return (
+
     <View style={styles.container}>
-      <Text>hello this is kimani</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.mystyles}>{user} is wearing {shoe.name} worth {shoe.price}</Text>
+       <TextInput style={styles.textinput}
+       placeholder='changename'
+       onChange={e=>setUser(e.target.value)}
+       />
+
+      <TextInput style={styles.textinput}
+       placeholder='change shoe'
+       onChange={e=>setShoe({...shoe, shoe: e.target.value})}
+       />
+        <TextInput style={styles.textinput}
+       placeholder='change price'
+       onChange={e=>setShoe({...shoe, price: e.target.value})}
+       multiline
+       />
+       
+      
+  <View style={{margin:10}}><Button style={styles.buttonStyle} onPress={changeUser} title='change'/></View>
     </View>
   );
 }
@@ -17,4 +43,27 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  mystyles: {
+    backgroundColor: 'yellow',
+  
+},
+buttonStyle: {
+  color: 'red',
+  marginTop: 20,
+  padding: 20,
+  backgroundColor: 'green'
+},
+textinput: {
+  borderWidth: 1,
+  borderColor: 'brown',
+  width: 300,
+  height: 50,
+  padding: 10,
+  margin: 10,
+  borderRadius: 20,
+
+
+}
+
+
 });
